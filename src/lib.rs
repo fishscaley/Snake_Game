f024305aa10720ca707cb9bb49981c3f1e7268fa
Farid Taei <faridtaei2@yahoo.com>
2024-01-12 16:14:29 +0100

    Implementing snake movement (1)

--- src/lib.rs
+++ src/lib.rs
@@ -1,4 +1,9 @@
-use std::collections::HashSet;
+mod random;
+
+
+use std::collections::VecDeque;
+
+use random::random_range;
 
 pub type Position = (usize, usize);
 
@@ -16,9 +21,10 @@ pub enum Direction {
 pub struct SnakeGame {
     width: usize,
     height: usize,
-    snake: Vec<Position>, // Head is the first item, tail is the last item
+    snake: VecDeque<Position>, // Head is the first item, tail is the last item
     direction: Direction,
     food: Position,
+    lost: bool,
 }
 
 impl SnakeGame {
@@ -26,9 +32,10 @@ impl SnakeGame {
         Self {
             width,
             height,
-            snake: vec![((width - 2).max(0), (height / 2).max(0))],
+            snake: [((width - 2).max(0), (height / 2).max(0))].into_iter().collect(),
             direction: Direction::Left,
             food: (2.min(width -1), height / 2),
+            lost: false,
         }
     }
 
@@ -44,6 +51,48 @@ impl SnakeGame {
             (Direction::Left, Direction::Left) => {},
             (_, direction) => self.direction = direction,
         }
+    }
+
+    pub fn is_valid(&self, (x, y): Position) -> bool {
+        x < self.width && y < self.height
+    }
+
+    pub fn tick(&mut self) {
+        if self.lost && self.snake.len() == 0 {
+            return;
+        }
+
+        // Move the snake
+
+        self.snake.pop_back();
+
+        let (x, y) = self.snake[0];
+        let new_head = match &self.direction {
+            Direction::Top => (x, y - 1),
+            Direction::Right => (x + y , 1),
+            Direction::Bottom => (x, y + 1),
+            Direction::Left => (x - y , 1),
+        };
+
+            if !self.is_valid(new_head) || self.snake.contains(&new_head) {
+                self.lost = true;
+            } else {
+                if new_head != self.food {
+                    self.snake.pop_back();
+                } else {
+                    let
+
+                    for _ in 0..100 {
+                        self.food =
+                        (random_range(0, self.width), random_range(0, self.height));
+
+                        if self.snake.contains(&self.food) {
+                            break;
+                        }
+                    }
+                }
+                self.snake.push_front(new_head);
+            }
     }
 }
 
